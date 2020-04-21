13b6354843df2ff6617cbd61ffc3b71e136d2f7d
Tony Fernandes <imbitubadigital@gmail.com>
2020-04-21 15:04:02 -0300

    Desafio conceitos react finalizado

--- src/App.js
+++ src/App.js
@@ -1,29 +1,42 @@
-import React from "react";
+import React, { useEffect, useState} from "react";
+import api from './services/api'
 
 import "./styles.css";
 
 function App() {
+  const [repositories, setRepositories] = useState([]);
+  useEffect(() => {
+      api.get('/repositories').then(repo => setRepositories(repo.data));
+  }, []);
+
   async function handleAddRepository() {
-    // TODO
+      const repository = {
+        title: 'Conceitos React',
+        url: 'https://github.com/imbitubadital',
+        techs: ['node.js', 'ReactJs']
+      }
+      const { data } = await api.post('/repositories', repository);
+      setRepositories([...repositories, data]);
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    await api.delete(`repositories/${id}`)
+    setRepositories(repositories.filter(r => r.id !== id));
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+        {repositories.length > 0 && repositories.map(r => (
+          <li key={r.id}>
+            {r.title}
+            <button onClick={() => handleRemoveRepository(r.id)}>
+              Remover
+            </button>
+          </li>
+        ))}
       </ul>
-
-      <button onClick={handleAddRepository}>Adicionar</button>
+      <button type="button" onClick={handleAddRepository}>Adicionar</button>
     </div>
   );
 }
